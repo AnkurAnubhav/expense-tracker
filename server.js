34aa6a22682e4c012e9f080d364e1752aacca85d
AnkurAnubhav <ankuranubhav1994@gmail.com>
2025-09-05 10:43:21 -0500

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 const expenseRoutes = require('./routes/expense');
 
 // Running express server
 const app = express();
 const port = process.env.PORT || 8000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, 'view/build')));
+
+// Catch all handler: send back React's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'view/build/index.html'));
+});
 
 // route middlewares
 app.use('/api', expenseRoutes);
